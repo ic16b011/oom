6671b26a45d9ee306d9efce25c569b24e5b71b33
Alexander Österreicher <ic16b011@technikum-wien.at>
2017-03-19 22:26:00 +0100

    add project Task4

---
+++ tasks/Task4/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Task4
+{
+    interface IClass
+    {
+        string Name { get; }
+        void setPlace(string newPlace);
+    }
+
+    public class Hobby : IClass
+    {
+        private int hobbieyears;
+
+        public Hobby(string name, string place)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new Exception("Name empty");
+            if (string.IsNullOrWhiteSpace(place)) throw new Exception("Place empty");
+
+            Name = name;
+            Place = place;
+        }
+
+        public string Name { get; }
+        public string Place { set; get; }
+
+        public void setPlace(string newPlace)
+        {
+            Place = newPlace;
+            Console.WriteLine("New Place Hobby: " + Place);
+        }
+
+        public void UpdateHobbyYears(int newyears)
+        {
+            if (newyears < 0) throw new ArgumentException("Years cannot be negative", newyears.ToString());
+            hobbieyears = newyears;
+            Console.WriteLine("Hobby Years: " + hobbieyears);
+        }
+    }
+
+    public class Friends : IClass
+    {
+        public Friends(string name, string place)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new Exception("Name empty");
+            if (string.IsNullOrWhiteSpace(place)) throw new Exception("Place empty");
+
+            Name = name;
+            Place = place;
+        }
+
+        public string Name { get; }
+        public string Place { set; get; }
+
+        public void setPlace(string newPlace)
+        {
+            if (string.IsNullOrWhiteSpace(newPlace)) throw new Exception("newPlace empty");
+
+            Place = newPlace;
+            Console.WriteLine("New Place Friend: " + Place);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var newHobby = new Hobby("Fußball", "Sportplatz");
+            var newHobby2 = new Hobby("Programmieren", "FH");
+            string newPlace;
+            int i = 0;
+
+            Console.WriteLine("Hobby Name: " + newHobby.Name);
+            Console.WriteLine("Hobby Place: " + newHobby.Place);
+            newHobby.UpdateHobbyYears(5);
+            newHobby.UpdateHobbyYears(8);
+
+            Console.Write("Set new Place: ");
+            newPlace = Console.ReadLine();
+            newHobby.setPlace(newPlace);
+
+            var newFriend = new Friends("Alex", "Wien");
+            var newFriend2 = new Friends("Bianca", "Wien");
+
+            var objects = new IClass[] { newHobby, newHobby2, newFriend, newFriend2 };
+            string s = JsonConvert.SerializeObject(objects);
+            Console.WriteLine(s);
+
+            while (i < objects.Count())
+            {
+                objects[i].setPlace("Wien_Test");
+                i++;
+            }
+
+            string json = JsonConvert.SerializeObject(newFriend);
+
+            Friends newFriend3 = JsonConvert.DeserializeObject<Friends>(json);
+            Console.WriteLine(newFriend.Name + " " + newFriend.Place);
+            Console.WriteLine(newFriend3.Name + " " + newFriend3.Place);
+
+            Console.Write("Zum Beenden Taste druecken");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ tasks/Task4/Tests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Task4
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void negativeYears()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Hobby("Test", "Test");
+                x.UpdateHobbyYears(-4);
+            });
+        }
+
+        [Test]
+        public void emptyName()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Hobby("", "Test");
+            });
+        }
+
+        [Test]
+        public void emptyPlace()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Hobby("Test", "");
+            });
+        }
+
+        [Test]
+        public void nameEqualsInit()
+        {
+            var x = new Hobby("Test", "Test2");
+            Assert.IsTrue(x.Name == "Test");
+        }
+
+        [Test]
+        public void placeEqualsInit()
+        {
+            var x = new Hobby("Test", "Test2");
+            Assert.IsTrue(x.Place == "Test2");
+        }
+
+        [Test]
+        public void friendplaceEqualsInit()
+        {
+            var x = new Friends("Test", "Test2");
+            Assert.IsTrue(x.Place == "Test2");
+        }
+
+        [Test]
+        public void friendnameEqualsInit()
+        {
+            var x = new Friends("Test", "Test2");
+            Assert.IsTrue(x.Name == "Test");
+        }
+
+        [Test]
+        public void emptyFriendPlace()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Friends("Test", "");
+            });
+        }
+
+        [Test]
+        public void emptyFriendName()
+        {
+            Assert.Catch(() =>
+            {
+                var x = new Friends("", "Test");
+            });
+        }
+    }
+}
